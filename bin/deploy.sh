dbf9156835c032671b0a0479e7dd07a0072f64db
Laurent <laurent.pi@gmail.com>
2025-02-07 13:00:41 +0100

    new sounds, better sounds

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 # Deploy config to VPS
-ROOTDIR=`pwd`"/..";
+
+cd "$(dirname "$0")"/..
 
 php bin/build-config.php && \
 rsync -zr --chown=asterisk:asterisk -e ssh build/  laurentpichler.com:/srv/phone-server && \
+bin/transcode-sounds-to-opus && \
+ssh laurentpichler.com "cp /srv/phone-server/usr/share/asterisk/sounds/en_US_f_Allison/*.opus /usr/share/asterisk/sounds/en_US_f_Allison"  && \
 ssh laurentpichler.com "sudo /bin/systemctl restart asterisk"
